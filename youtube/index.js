09322b3d5092bb9c2cacec01b522019544927009
sandeepkadangode.ms@gmail.com <sandeep>
2023-02-01 18:42:45 +0530

    youtube clone finished

---
+++ youtube/index.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu');
+const sidebar = document.querySelector('.sidebar');
+
+menu.addEventListener('click', function(){
+    sidebar.classList.toggle('show-sidebar');
+})
